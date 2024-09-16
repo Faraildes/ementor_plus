82886f5339aa796efa6c7682258266d3170850f0
Faraildes <faraildesbarbosaferreira@gmail.com>
2024-09-16 10:04:42 -0300

    Observer design pattern to update tbleview

--- src/gui/TeacherFormController.java
+++ src/gui/TeacherFormController.java
@@ -4,11 +4,14 @@ import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -28,6 +31,8 @@ public class TeacherFormController implements Initializable {
 	private Teacher entity;
 	
 	private TeacherService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -88,6 +93,10 @@ public class TeacherFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
 	@FXML
 	private void onBtSaveAction(ActionEvent event) {
 		if(entity == null)
@@ -97,6 +106,7 @@ public class TeacherFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpadate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -104,6 +114,12 @@ public class TeacherFormController implements Initializable {
 		}
 	}
 		
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}		
+	}
+
 	private Teacher getFormData() {
 		Teacher obj = new Teacher();
 		
@@ -121,8 +137,8 @@ public class TeacherFormController implements Initializable {
 	}
 
 	@FXML
-	private void onBtCancelAction() {
-		System.out.println("onBtCancelAction");
+	private void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 			
 	}
 	@Override
--- src/gui/TeacherListController.java
+++ src/gui/TeacherListController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -27,7 +28,7 @@ import javafx.stage.Stage;
 import model.entities.Teacher;
 import model.services.TeacherService;
 
-public class TeacherListController implements Initializable{
+public class TeacherListController implements Initializable, DataChangeListener{
 	
 	private TeacherService service;
 	
@@ -109,6 +110,7 @@ public class TeacherListController implements Initializable{
 			TeacherFormController controller = loader.getController();
 			controller.setTeacher(obj);
 			controller.setTeacherService(new TeacherService());
+			controller.subscribeDataChangeListener(this);;
 			controller.updateFormData();
 			
 			Stage dialogStage = new Stage();
@@ -123,5 +125,10 @@ public class TeacherListController implements Initializable{
 		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error lçoading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		updateTableView();		
 	}
 }
